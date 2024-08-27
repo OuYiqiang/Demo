2fd5df9d0de6432de8b48e218cf85e33497eed2f
oyq1999 <1185789422@qq.com>
2024-08-27 22:54:43 +0800

    音频数据内容js

---
+++ wavData.js
@@ -0,0 +1,106 @@
+export const MultiData = [// 在这里插入音频和对应内容
+    {
+        text: "这是一条用于语音合成的音频",
+        audioFiles: {
+            neutral: "./src/wave/multi-tacotron2/这是一条用于语音合成的音频/这是一条用于语音合成的音频_neu.wav",
+            happy: "./src/wave/multi-tacotron2/这是一条用于语音合成的音频/这是一条用于语音合成的音频_hap.wav",
+            sad: "./src/wave/multi-tacotron2/这是一条用于语音合成的音频/这是一条用于语音合成的音频_sad.wav",
+            surprised: "./src/wave/multi-tacotron2/这是一条用于语音合成的音频/这是一条用于语音合成的音频_sur.wav",
+            angry: "./src/wave/multi-tacotron2/这是一条用于语音合成的音频/这是一条用于语音合成的音频_ang.wav"
+        }
+    },
+    {
+        text: "今天天气真好",
+        audioFiles: {
+            neutral: "./src/wave/multi-tacotron2/今天天气真好/今天天气真好_neu.wav",
+            happy: "./src/wave/multi-tacotron2/今天天气真好/今天天气真好_hap.wav",
+            sad: "./src/wave/multi-tacotron2/今天天气真好/今天天气真好_sad.wav",
+            surprised: "./src/wave/multi-tacotron2/今天天气真好/今天天气真好_sur.wav",
+            angry: "./src/wave/multi-tacotron2/今天天气真好/今天天气真好_ang.wav"
+        }
+    },
+    {
+        text: "我认为意大利面就应该拌四十二号混凝土",
+        audioFiles: {
+            neutral: "./src/wave/multi-tacotron2/我认为意大利面就应该拌四十二号混凝土/我认为意大利面就应该拌四十二号混凝土_neu.wav",
+            happy: "./src/wave/multi-tacotron2/我认为意大利面就应该拌四十二号混凝土/我认为意大利面就应该拌四十二号混凝土_hap.wav",
+            sad: "./src/wave/multi-tacotron2/我认为意大利面就应该拌四十二号混凝土/我认为意大利面就应该拌四十二号混凝土_sad.wav",
+            surprised: "./src/wave/multi-tacotron2/我认为意大利面就应该拌四十二号混凝土/我认为意大利面就应该拌四十二号混凝土_sur.wav",
+            angry: "./src/wave/multi-tacotron2/我认为意大利面就应该拌四十二号混凝土/我认为意大利面就应该拌四十二号混凝土_ang.wav"
+        }
+    },
+    {
+        text: "我好想退休回农村盖房养猪种地",
+        audioFiles: {
+            neutral: "./src/wave/multi-tacotron2/我好想退休回农村盖房养猪种地/我好想退休回农村盖房养猪种地_neu.wav",
+            happy: "./src/wave/multi-tacotron2/我好想退休回农村盖房养猪种地/我好想退休回农村盖房养猪种地_hap.wav",
+            sad: "./src/wave/multi-tacotron2/我好想退休回农村盖房养猪种地/我好想退休回农村盖房养猪种地_sad.wav",
+            surprised: "./src/wave/multi-tacotron2/我好想退休回农村盖房养猪种地/我好想退休回农村盖房养猪种地_sur.wav",
+            angry: "./src/wave/multi-tacotron2/我好想退休回农村盖房养猪种地/我好想退休回农村盖房养猪种地_ang.wav"
+        }
+    },
+    {
+        text: "我发誓,下个月一定去减肥",
+        audioFiles: {
+            neutral: "./src/wave/multi-tacotron2/我发誓下个月一定去减肥/我发誓下个月一定去减肥_neu.wav",
+            happy: "./src/wave/multi-tacotron2/我发誓下个月一定去减肥/我发誓下个月一定去减肥_hap.wav",
+            sad: "./src/wave/multi-tacotron2/我发誓下个月一定去减肥/我发誓下个月一定去减肥_sad.wav",
+            surprised: "./src/wave/multi-tacotron2/我发誓下个月一定去减肥/我发誓下个月一定去减肥_sur.wav",
+            angry: "./src/wave/multi-tacotron2/我发誓下个月一定去减肥/我发誓下个月一定去减肥_ang.wav"
+        }
+    },
+    // 你可以在这里继续添加更多的文本和音频数据
+];
+export const VaeData = [// 在这里插入音频和对应内容
+    {
+        text: "这是一条用于语音合成的音频",
+        audioFiles: {
+            neutral: "./src/wave/bert_vits_1.wav",
+            happy: "audio1_happy.mp3",
+            sad: "audio1_sad.mp3",
+            surprised: "audio1_surprised.mp3",
+            angry: "audio1_angry.mp3"
+        }
+    },
+    {
+        text: "今天天气真好",
+        audioFiles: {
+            neutral: "audio2_neutral.mp3",
+            happy: "audio2_happy.mp3",
+            sad: "audio2_sad.mp3",
+            surprised: "audio2_surprised.mp3",
+            angry: "audio2_angry.mp3"
+        }
+    },
+    {
+        text: "我认为意大利面就应该拌四十二号混凝土",
+        audioFiles: {
+            neutral: "audio2_neutral.mp3",
+            happy: "audio2_happy.mp3",
+            sad: "audio2_sad.mp3",
+            surprised: "audio2_surprised.mp3",
+            angry: "audio2_angry.mp3"
+        }
+    },
+    {
+        text: "我好想退休回农村盖房养猪种地我发誓，下个月一定减肥",
+        audioFiles: {
+            neutral: "audio2_neutral.mp3",
+            happy: "audio2_happy.mp3",
+            sad: "audio2_sad.mp3",
+            surprised: "audio2_surprised.mp3",
+            angry: "audio2_angry.mp3"
+        }
+    },
+    {
+        text: "我发誓，下个月一定去减肥",
+        audioFiles: {
+            neutral: "audio2_neutral.mp3",
+            happy: "audio2_happy.mp3",
+            sad: "audio2_sad.mp3",
+            surprised: "audio2_surprised.mp3",
+            angry: "audio2_angry.mp3"
+        }
+    },
+    // 你可以在这里继续添加更多的文本和音频数据
+];
